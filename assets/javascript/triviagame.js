7e4c2eb80310339906bb3cbb6fa4e11ab3099498
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-10 08:37:56 -0400

    Aug10-0837

--- assets/javascript/triviagame.js
+++ assets/javascript/triviagame.js
@@ -3,13 +3,19 @@ var numberWrong = 0;
 var numbWins = 0;
 var numbLoses = 0;
 var q1 = {
-	question: "who hosted queen for a day",
+	question: "Who hosted queen for a day?",
 	possibleAnswer1: "Johnny Carson",
 	possibleAnswer2: "Jack Bailey",
 	possibleAnswer3: "ED McMahon",
 	correctAnswer: "Jack Bailey"
 }
-
+var q2 = {
+	question: "Who wqs the first host of the tonight show?",
+	passibleAnswer1: "Johnny Carson",
+	possibleAnswer2: "Joey Bishop",
+	possibeAnswer3: "Jack Par",
+	correctAnswer: "Jack Par"
+}
 function initialize() {
 
 
