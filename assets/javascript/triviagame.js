18e00c99f8923d4d8a6b5919630b33ddd5e62f8c
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-10 08:33:01 -0400

    Aug10-08:32

--- assets/javascript/triviagame.js
+++ assets/javascript/triviagame.js
@@ -2,6 +2,13 @@ var numberCorrect = 0;
 var numberWrong = 0;
 var numbWins = 0;
 var numbLoses = 0;
+var q1 = {
+	question: "who hosted queen for a day",
+	possibleAnswer1: "Johnny Carson",
+	possibleAnswer2: "Jack Bailey",
+	possibleAnswer3: "ED McMahon",
+	correctAnswer: "Jack Bailey"
+}
 
 function initialize() {
 
