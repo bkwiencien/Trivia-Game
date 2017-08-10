afa40615f825182643552fba4e5e86ffeb60bf7a
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-10 11:03:28 -0400

    Aug10-11:03

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,22 @@ var q1 = {
 	correctAnswer: "Jack Bailey"
 }
 var q2 = {
-	question: "Who wqs the first host of the tonight show?",
+	question: "Who was the first host of the tonight show?",
 	passibleAnswer1: "Johnny Carson",
 	possibleAnswer2: "Joey Bishop",
-	possibeAnswer3: "Jack Par",
-	correctAnswer: "Jack Par"
+	possibeAnswer3: "Jack Paar",
+	correctAnswer: "Jack Paar"
+}
+var q3 = {
+	question: "Daddy im Kare Room for Daddy was played by?",
+	passibleAnswer1: "Johnny Carson",
+	possibleAnswer2: "Frank Sinatra",
+	possibeAnswer3: "Danny Thomas",
+	correctAnswer: "Danny Thomas"
 }
 function initialize() {
 	console.log("initialize");
+	createQuestions();
 	var timeLeft = 121;
 	console.log("time left = " + timeLeft);
 	var gameTimer = setInterval(function() {
@@ -28,5 +36,8 @@ function initialize() {
 			$("#timer").html("seconds Remaining: 0");
 		}
 	},1000);
+
+}
+function createQuestions() {
 
 }
