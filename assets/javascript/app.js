f7d28cb316ad3d7e78aee8d4aceadb80a2703e57
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-13 12:49:38 -0400

    Aug13-12:49

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -88,6 +88,9 @@ function initialize() {
 		if (timeLeft == 5){
 			$("#tv").attr("src","assets/images/tv8.jpeg");
 		}
+		if (timeLeft == 3){
+			$("#tv").attr("src","assets/images/tv12.jpeg");
+		}
 		if (timeLeft <= 0){
 			clearInterval(gameTimer);
 			assesTheGame();
