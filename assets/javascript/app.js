f780be8b0ad5a2a375b3be6d704cf0c0c60861b7
Bob Kwiencien <bkwiencien@gmail.com>
2017-08-13 12:45:40 -0400

    Aug13-12:45

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -82,6 +82,9 @@ function initialize() {
 		if (timeLeft == 10){
 			$("#tv").attr("src","assets/images/tv4.jpeg");
 		}
+		if (timeLeft == 7){
+			$("#tv").attr("src","assets/images/tv11.jpeg");
+		}
 		if (timeLeft == 5){
 			$("#tv").attr("src","assets/images/tv8.jpeg");
 		}
